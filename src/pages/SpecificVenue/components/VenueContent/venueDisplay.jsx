68d89f4f18c7fca7aa5981e6124e3121a2dfbdb3
Marius Rundereim <marius.rundereim@gmail.com>
2024-04-25 12:30:29 +0200

    added a date fns npm package

--- src/pages/SpecificVenue/components/VenueContent/venueDisplay.jsx
+++ src/pages/SpecificVenue/components/VenueContent/venueDisplay.jsx
@@ -1,12 +1,15 @@
 import { Title, Text } from "@mantine/core";
+import formatCurrency from "../../../../utils/format/currencyFormat";
 function VenueDisplay({ venue }) {
-  const { name, description, created, maxGuests } = venue;
+  const { name, description, created, maxGuests, price } = venue;
+  const formattedPrice = formatCurrency(price);
   return (
     <>
       <Title>{name}</Title>
       <h2>Venue Information</h2>
       <Text>Description: {description}</Text>
       <Text>{created}</Text>
+      <Text>Price: {formattedPrice}</Text>
     </>
   );
 }
