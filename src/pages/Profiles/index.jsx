805d153cf93736b072520a975521830310b4dcd3
Marius Rundereim <marius.rundereim@gmail.com>
2024-05-05 12:56:35 +0200

    starting with update api to handle meta

--- src/pages/Profiles/index.jsx
+++ src/pages/Profiles/index.jsx
@@ -1,23 +1,29 @@
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllProfiles } from "../../store/profiles/profilesSlice";
-import { useEffect } from "react";
 import ListProfiles from "./listProfiles";
 import SearchPanelProfiles from "./SearchBar";
+import PaginationProfiles from "./PaginationControls";
 
 function Profiles() {
   const dispatch = useDispatch();
-
+  const [currentPage, setCurrentPage] = useState(1);
   const allProfiles = useSelector((state) => state.profiles.allProfilesList);
 
   useEffect(() => {
-    dispatch(getAllProfiles());
-  }, [dispatch]);
+    dispatch(getAllProfiles(currentPage));
+  }, [dispatch, currentPage]);
+
+  const handlePageChange = (newPage) => {
+    setCurrentPage(newPage);
+  };
 
   return (
     <>
       <h2>All profiles</h2>
       <SearchPanelProfiles />
       <ListProfiles profiles={allProfiles} />
+      <PaginationProfiles />
     </>
   );
 }
