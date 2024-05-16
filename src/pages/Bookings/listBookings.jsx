0be728d0f63cd859580abd058f8c8f695102f96b
Marius Rundereim <marius.rundereim@gmail.com>
2024-05-16 15:50:08 +0200

    some bugs with register form when register a profile

--- src/pages/Bookings/listBookings.jsx
+++ src/pages/Bookings/listBookings.jsx
@@ -3,7 +3,7 @@ import CustomerBookingCard from "../../components/cards/CustomerBookingCard";
 function ListBookings({ bookings }) {
   return (
     <>
-      <Title>Bookings</Title>
+      <Title>Your Upcoming booking as </Title>
       <Grid>
         {bookings.map((booking) => (
           <Grid.Col span={{ base: 12, md: 6, lg: 3 }} key={booking.id}>
