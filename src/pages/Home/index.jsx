cb4b701efdb6737ee2f20751685eb1e82250fad1
Marius Rundereim <marius.rundereim@gmail.com>
2024-05-21 16:31:39 +0200

    not updated

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,10 +3,10 @@ function Home() {
   return (
     <>
       <Title order={1}>Solbakken Resort</Title>
-      <Title order={2}>Home</Title>
-      <Title order={3}>Home</Title>
-      <Title order={4}>Home</Title>
-      <Text>text</Text>
+      <Title order={2}>Title order 2</Title>
+      <Title order={3}>Title order 3</Title>
+      <Title order={4}>Title order 4</Title>
+      <Text>Paragraph text</Text>
     </>
   );
 }
