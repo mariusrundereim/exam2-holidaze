ba9cdeb080b513b62b5304312991b4edd54c361e
Marius Rundereim <marius.rundereim@gmail.com>
2024-05-26 17:17:06 +0200

    update booking

--- src/components/cards/BookingVenueCard/index.jsx
+++ src/components/cards/BookingVenueCard/index.jsx
@@ -1,13 +1,47 @@
-import { Card, Title, Text } from "@mantine/core";
+import { Grid, Card, Title, Text } from "@mantine/core";
 import formatCurrency from "../../../utils/format/currencyFormat";
-
-function BookingVenueCard({ venue }) {
+import { useEffect, useState } from "react";
+import { calculateTotalPrice } from "../../../utils/booking/calculateTotalPrice";
+function BookingVenueCard({ venue, bookingDates }) {
   const { price, name, description, created } = venue;
+  console.log("venue", venue);
+  const [totalPrice, setTotalPrice] = useState(0);
+  const [nights, setNights] = useState(0);
+
+  useEffect(() => {
+    if (bookingDates[0] && bookingDates[1]) {
+      const { totalPrice, nights } = calculateTotalPrice(
+        bookingDates[0],
+        bookingDates[1],
+        price
+      );
+      setTotalPrice(totalPrice);
+      setNights(nights);
+    }
+  }, [bookingDates, price]);
+
   return (
     <>
       <Card>
-        <Title order={3}>Book on {name}</Title>
-        <Text>{formatCurrency(price)}</Text>
+        <Card.Section py={20} withBorder>
+          <Title order={3}>Book on {name}</Title>
+        </Card.Section>
+        <Card.Section py={20} withBorder>
+          <Grid>
+            <Grid.Col span={4}>
+              <Title order={4}>Price per night</Title>
+              <Text>{formatCurrency(price)}</Text>
+            </Grid.Col>
+            <Grid.Col span={4}>
+              <Title order={4}>Total price</Title>
+              <Text>{formatCurrency(totalPrice)}</Text>
+            </Grid.Col>
+          </Grid>
+        </Card.Section>
+        <Card.Section py={20} withBorder>
+          <Title order={4}>Days</Title>
+          <Text>Sleeping days: {nights}</Text>
+        </Card.Section>
       </Card>
     </>
   );
