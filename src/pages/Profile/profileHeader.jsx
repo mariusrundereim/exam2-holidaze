b84ea814da8ac6e9e3c59bcf283d364d57c00722
Marius Rundereim <marius.rundereim@gmail.com>
2024-05-28 19:27:21 +0200

    update global header

--- src/pages/Profile/profileHeader.jsx
+++ src/pages/Profile/profileHeader.jsx
@@ -20,6 +20,7 @@ import { useEffect } from "react";
 function ProfileHeader() {
   const dispatch = useDispatch();
   const profile = useSelector((state) => state.profile);
+  console.log("ppp", profile);
 
   const { name, email, bio, venueManager, avatar = {}, banner = {} } = profile;
   useEffect(() => {
@@ -32,27 +33,35 @@ function ProfileHeader() {
     <>
       <Grid>
         <Grid.Col>
-          <Image src={banner.url} radius="md" h={100} />
+          <Image src={banner.url} radius="md" h={{ lg: 440, sm: 240 }} />
         </Grid.Col>
         <Grid.Col>
-          <Title order={3}>Welcome back, {name}!</Title>
+          <Title order={2}>Hello, {name}!</Title>
         </Grid.Col>
         <Grid.Col>
-          <Avatar src={avatar.url} size="xl" />
-          <Stack align="center">
-            <Badge>{role}</Badge>
-          </Stack>
-        </Grid.Col>
-        <Grid.Col></Grid.Col>
-        <Grid.Col>
-          <Card shadow="sm" p={20} radius="md" withBorder>
-            <Card.Section>
-              <Title order={4}>Biography</Title>
-            </Card.Section>
-            <Card.Section>
-              <Text>{bio}</Text>
-            </Card.Section>
-          </Card>
+          <Flex direction={"column"} gap="md">
+            <Card withBorder>
+              <Card.Section p={10}>
+                <Text fw={500}>{name}</Text>
+                <Text>{email}</Text>
+                <Badge>{role}</Badge>
+                <Avatar src={avatar.url} size="lg" />
+              </Card.Section>
+              <Card.Section></Card.Section>
+            </Card>
+            <Card withBorder>
+              <Card.Section p={10}>
+                <Title order={4}>Email:</Title>
+                <Text>{email}</Text>
+              </Card.Section>
+            </Card>
+            <Card withBorder>
+              <Card.Section p={10}>
+                <Title order={4}>Biography</Title>
+                <Text>{bio || "Add biography on settings"}</Text>
+              </Card.Section>
+            </Card>
+          </Flex>
         </Grid.Col>
       </Grid>
     </>
