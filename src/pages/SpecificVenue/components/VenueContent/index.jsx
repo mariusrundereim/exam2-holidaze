b0aafab0cc4f467bf942bf8d014a8d017c8c29e6
Marius Rundereim <marius.rundereim@gmail.com>
2024-05-28 23:32:33 +0200

    split up code better than first time on venues specific

--- src/pages/SpecificVenue/components/VenueContent/index.jsx
+++ src/pages/SpecificVenue/components/VenueContent/index.jsx
@@ -3,18 +3,27 @@ import LocationDisplay from "./locationDisplay";
 import AmenitiesDisplay from "./amenitiesDisplay";
 import VenueDisplay from "./venueDisplay";
 import OwnerDisplay from "./ownerDisplay";
+import DetailsVenue from "./detailsDisplay";
+import DescriptionVenue from "./descDisplay";
 function VenueContent({ venue }) {
   const { name, location, meta } = venue;
   console.log("vvv", venue);
   return (
     <>
-      <Grid gutter={100}>
+      <Grid gutter={{ base: 60, md: 60, sm: 20 }}>
         <Grid.Col>
           <VenueDisplay venue={venue} />
         </Grid.Col>
         <Grid.Col>
+          <DescriptionVenue venue={venue} />
+        </Grid.Col>
+        <Grid.Col>
+          <DetailsVenue venue={venue} />
+        </Grid.Col>
+        <Grid.Col>
           <LocationDisplay location={location} />
         </Grid.Col>
+
         <Grid.Col>
           <AmenitiesDisplay meta={meta} />
         </Grid.Col>
--- src/pages/SpecificVenue/components/VenueContent/venueDisplay.jsx
+++ src/pages/SpecificVenue/components/VenueContent/venueDisplay.jsx
@@ -1,18 +1,14 @@
-import { Grid, Title, Text, Button, Group, Stack } from "@mantine/core";
-import formatCurrency from "../../../../utils/format/currencyFormat";
-import { formattedDate } from "../../../../utils/format/dateFormat";
+import { Grid, Title, Group } from "@mantine/core";
 import BookingButton from "../../../../components/venues/BookingButton";
 import { useParams } from "react-router-dom";
 import GalleryPicturesVenue from "../Gallery";
 function VenueDisplay({ venue }) {
-  const { name, description, created, maxGuests, price, media } = venue;
-  const formattedPrice = formatCurrency(price);
-  const newDate = formattedDate(created);
+  const { name, media } = venue;
   const { venueId } = useParams();
 
   return (
     <>
-      <Grid gutter={80}>
+      <Grid>
         <Grid.Col my={30}>
           <Title>{name}</Title>
         </Grid.Col>
@@ -21,23 +17,6 @@ function VenueDisplay({ venue }) {
             <BookingButton venueId={venueId} />
             <GalleryPicturesVenue venueId={venueId} media={media} />
           </Group>
-        </Grid.Col>
-        <Grid.Col>
-          <Title order={4} mb={10}>
-            Description
-          </Title>
-          <Text>{description}</Text>
-        </Grid.Col>
-        <Grid.Col span={6}>
-          <Title order={4} mb={10}>
-            Information
-          </Title>
-
-          <Stack gap="xs">
-            <Text size="xl">{formattedPrice} /night</Text>
-            <Text size="xl">{maxGuests} guests</Text>
-            <Text size="xl">Created: {newDate}</Text>
-          </Stack>
         </Grid.Col>
       </Grid>
     </>
