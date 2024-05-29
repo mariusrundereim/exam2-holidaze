809e30324fd6a74dcc7e1a7a60ab890405bbf7b8
Marius Rundereim <marius.rundereim@gmail.com>
2024-05-29 09:32:52 +0200

    update venue manager booking cards

--- src/components/cards/VenueManagerBookingCard/index.jsx
+++ src/components/cards/VenueManagerBookingCard/index.jsx
@@ -6,7 +6,7 @@ import {
   IconHash,
   IconUserQuestion,
 } from "@tabler/icons-react";
-import { Card, Title, Text, Group } from "@mantine/core";
+import { Card, Title, Text, Group, Stack, Grid } from "@mantine/core";
 import { formattedDateTime } from "../../../utils/format/dateFormat";
 function VenueManagerBookingCard({ booking }) {
   const {
@@ -21,35 +21,33 @@ function VenueManagerBookingCard({ booking }) {
   return (
     <>
       <Card shadow="sm" p="lg" withBorder>
-        <Card.Section>
-          <Group>
-            <IconUserCircle />
-            <Text>{name}</Text>
-          </Group>
-          <Group>
-            <IconAt />
-            <Text>{email}</Text>
-          </Group>
+        <Card.Section p={10}>
+          <Grid>
+            <Grid.Col>
+              <Text fw={600}>{name}</Text>
+              <Text>{email}</Text>
+            </Grid.Col>
+          </Grid>
         </Card.Section>
-        <Card.Section mt="md">
+        <Card.Section p={10}>
           <Group>
-            <IconHomePlus />
+            <IconHomePlus size={20} />
             <Text>{formattedDateTime(created)}</Text>
           </Group>
           <Group>
-            <IconMobiledata />
+            <IconMobiledata size={20} />
             <Text>
               {formattedDateTime(dateFrom)} - {formattedDateTime(dateTo)}
             </Text>
           </Group>
         </Card.Section>
-        <Card.Section>
+        <Card.Section p={10}>
           <Group>
-            <IconHash />
+            <IconHash size={20} />
             <Text>{id}</Text>
           </Group>
           <Group>
-            <IconUserQuestion />
+            <IconUserQuestion size={20} />
             <Text>{guests} guests</Text>
           </Group>
         </Card.Section>
--- src/pages/Bookings/manager/managerListBookings.jsx
+++ src/pages/Bookings/manager/managerListBookings.jsx
@@ -1,33 +1,37 @@
-import { Grid, Title, Text, Card } from "@mantine/core";
+import { Grid, Title, Text, Card, Container } from "@mantine/core";
 import VenueManagerBookingCard from "../../../components/cards/VenueManagerBookingCard";
 function VenueManagerBookings({ bookings }) {
   return (
     <>
-      <Title>Venue Manager Bookings</Title>
-      <Grid>
-        {bookings.map((venue) => (
-          <Grid.Col span={{ base: 12 }} key={venue.id}>
-            <Card shadow="sm" p="lg" withBorder>
-              <Title order={3}>{venue.name}</Title>
-              <Text>
-                {venue.location.address} - {venue.location.city}
-              </Text>
+      <Container>
+        <Title order={3} mb={30}>
+          Venue Manager Bookings
+        </Title>
+        <Grid>
+          {bookings.map((venue) => (
+            <Grid.Col span={{ base: 12 }} key={venue.id}>
+              <Card withBorder>
+                <Title order={4}>{venue.name}</Title>
+                <Text>
+                  {venue.location.address} - {venue.location.city}
+                </Text>
 
-              {venue.bookings.length > 0 ? (
-                <Grid mt="md">
-                  {venue.bookings.map((booking) => (
-                    <Grid.Col span={12} key={booking.id}>
-                      <VenueManagerBookingCard booking={booking} />
-                    </Grid.Col>
-                  ))}
-                </Grid>
-              ) : (
-                <Text mt="md">No booking yet!</Text>
-              )}
-            </Card>
-          </Grid.Col>
-        ))}
-      </Grid>
+                {venue.bookings.length > 0 ? (
+                  <Grid mt="md">
+                    {venue.bookings.map((booking) => (
+                      <Grid.Col span={12} key={booking.id}>
+                        <VenueManagerBookingCard booking={booking} />
+                      </Grid.Col>
+                    ))}
+                  </Grid>
+                ) : (
+                  <Text mt="md">No booking yet!</Text>
+                )}
+              </Card>
+            </Grid.Col>
+          ))}
+        </Grid>
+      </Container>
     </>
   );
 }
