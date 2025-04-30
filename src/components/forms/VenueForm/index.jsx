c8f4a3103ba43608ae1f75ba6a91594a20b2b468
Marius <marius.rundereim@gmail.com>
2025-04-30 15:03:54 +0200

    update create venue with stepper

--- src/components/forms/VenueForm/index.jsx
+++ src/components/forms/VenueForm/index.jsx
@@ -19,6 +19,8 @@ import {
   Title,
   Text,
   Container,
+  Stepper,
+  Group,
 } from "@mantine/core";
 import SelectCityAndCountry from "./selectCityAndCountry";
 import { IconPhoto, IconTypography } from "@tabler/icons-react";
@@ -26,6 +28,12 @@ function VenueForm() {
   const { venueId } = useParams();
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const [active, setActive] = useState(0);
+
+  const nextStep = () =>
+    setActive((current) => (current < 3 ? current + 1 : current));
+  const prevStep = () =>
+    setActive((current) => (current > 0 ? current - 1 : current));
 
   const venue = useSelector((state) => state.venues.selectedVenue);
 
@@ -134,228 +142,278 @@ function VenueForm() {
   return (
     <>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <Grid gutter={40}>
-          <Grid.Col span={{ base: 12, md: 6 }}>
-            <Controller
-              name="name"
-              control={control}
-              rules={{ required: true }}
-              render={({ field }) => (
-                <Input.Wrapper label="Name" description="Enter name of a venue">
-                  <Input
-                    {...field}
-                    placeholder="Name"
-                    value={field.value ?? ""}
-                  />
-                </Input.Wrapper>
-              )}
-            />
-            {errors.name && <Text>Name is required</Text>}
-            <Controller
-              name="price"
-              control={control}
-              rules={{ required: true }}
-              render={({ field }) => (
-                <Input.Wrapper
-                  label="Price"
-                  description="Give a price"
-                  min={0}
-                  withAsterisk
-                >
-                  <NumberInput {...field} />
-                </Input.Wrapper>
-              )}
-            />
-            {errors.price && <Text>Price is required</Text>}
-            <Controller
-              name="maxGuests"
-              control={control}
-              rules={{ required: true }}
-              render={({ field }) => (
-                <Input.Wrapper
-                  label="Max guests"
-                  description="How many?"
-                  withAsterisk
-                >
-                  <NumberInput {...field} />
-                </Input.Wrapper>
-              )}
-            />
-            {errors.maxGuests && <Text>Guests is required</Text>}
-          </Grid.Col>
-          <Grid.Col span={{ base: 12, md: 6 }}>
-            <Controller
-              name="description"
-              control={control}
-              rules={{ required: true }}
-              render={({ field }) => (
-                <Textarea
-                  {...field}
-                  label="Description"
-                  description="Describe your venue"
-                  autosize
-                  minRows={8}
-                  maxRows={10}
-                  withAsterisk
-                />
-              )}
-            />
-            {errors.description && <Text>Description is required</Text>}
-          </Grid.Col>
-          <Grid.Col span={6}>
-            <Title order={4}>Facilities</Title>
-            <Text>Select features</Text>
-            <Stack>
-              <Controller
-                name="meta.wifi"
-                control={control}
-                render={({ field: { value } }) => (
-                  <Switch
-                    label="Wifi"
-                    size="lg"
-                    onLabel="Yes"
-                    offLabel="No"
-                    checked={meta.wifi}
-                    onChange={(event) =>
-                      handleMetaChange("wifi", event.currentTarget.checked)
-                    }
-                  />
-                )}
-              />
-
-              <Controller
-                name="meta.parking"
-                control={control}
-                render={({ field: { value } }) => (
-                  <Switch
-                    label="Parking"
-                    size="lg"
-                    onLabel="Yes"
-                    offLabel="No"
-                    checked={meta.parking}
-                    onChange={(event) =>
-                      handleMetaChange("parking", event.currentTarget.checked)
-                    }
-                  />
-                )}
-              />
-              <Controller
-                name="meta.breakfast"
-                control={control}
-                render={({ field: { value } }) => (
-                  <Switch
-                    label="Breakfast"
-                    size="lg"
-                    onLabel="Yes"
-                    offLabel="No"
-                    checked={meta.breakfast}
-                    onChange={(event) =>
-                      handleMetaChange("breakfast", event.currentTarget.checked)
-                    }
-                  />
-                )}
-              />
-              <Controller
-                name="meta.pets"
-                control={control}
-                render={({ field: { value } }) => (
-                  <Switch
-                    label="Pets"
-                    size="lg"
-                    onLabel="Yes"
-                    offLabel="No"
-                    checked={meta.pets}
-                    onChange={(event) =>
-                      handleMetaChange("pets", event.currentTarget.checked)
-                    }
-                  />
-                )}
-              />
-            </Stack>
-          </Grid.Col>
-          <Grid.Col span={{ base: 12, sm: 6 }}>
-            <Stack>
-              <SelectCityAndCountry control={control} setValue={setValue} />
-              <Controller
-                name="location.address"
-                control={control}
-                rules={{ required: false }}
-                render={({ field }) => (
-                  <Input.Wrapper label="Address">
-                    <Input {...field} placeholder="Address" />
-                  </Input.Wrapper>
-                )}
-              />
-            </Stack>
-          </Grid.Col>
-          <Grid.Col>
-            <Title order={4}>Pictures</Title>
-            <Text>Up to 6 pictures. </Text>
-            {fields.map((field, index) => (
-              <Grid key={field.id} gutter={10}>
-                <Grid.Col span={{ base: 12, md: 6 }}>
-                  <Stack>
-                    <Controller
-                      name={`media[${index}].url`}
-                      control={control}
-                      rules={{ required: "URL is required" }} // Add required rule with a message
-                      render={({ field, fieldState: { error } }) => (
-                        <Input.Wrapper label={`Picture URL #${index + 1}`}>
-                          <Input
-                            leftSection={<IconPhoto size={20} />}
-                            placeholder="Enter media URL"
-                            {...field}
-                          />
-                          {error && <Text>{error.message}</Text>}
-                        </Input.Wrapper>
-                      )}
-                    />
-                    <Button
-                      variant="outline"
-                      color="red"
-                      fullWidth
-                      onClick={() => remove(index)}
-                      disabled={fields.length === 1}
+        <Stepper
+          active={active}
+          onStepClick={setActive}
+          allowNextStepsSelect={false}
+        >
+          <Stepper.Step
+            label="Basic Info"
+            description="Name, Price, Guests, Description"
+          >
+            <Grid gutter={40}>
+              {/* Step 0 - Name, Price, Max Guests, Description*/}
+              <Grid.Col span={{ base: 12, md: 6 }}>
+                <Controller
+                  name="name"
+                  control={control}
+                  rules={{ required: true }}
+                  render={({ field }) => (
+                    <Input.Wrapper
+                      label="Name"
+                      description="Enter name of a venue"
                     >
-                      Remove
-                    </Button>
-                  </Stack>
-                </Grid.Col>
-                <Grid.Col span={{ base: 12, md: 6 }}>
+                      <Input
+                        {...field}
+                        placeholder="Name"
+                        value={field.value ?? ""}
+                      />
+                    </Input.Wrapper>
+                  )}
+                />
+                {errors.name && <Text>Name is required</Text>}
+                <Controller
+                  name="price"
+                  control={control}
+                  rules={{ required: true }}
+                  render={({ field }) => (
+                    <Input.Wrapper
+                      label="Price"
+                      description="Give a price"
+                      min={0}
+                      withAsterisk
+                    >
+                      <NumberInput {...field} />
+                    </Input.Wrapper>
+                  )}
+                />
+                {errors.price && <Text>Price is required</Text>}
+                <Controller
+                  name="maxGuests"
+                  control={control}
+                  rules={{ required: true }}
+                  render={({ field }) => (
+                    <Input.Wrapper
+                      label="Max guests"
+                      description="How many?"
+                      withAsterisk
+                    >
+                      <NumberInput {...field} />
+                    </Input.Wrapper>
+                  )}
+                />
+                {errors.maxGuests && <Text>Guests is required</Text>}
+              </Grid.Col>
+              <Grid.Col span={{ base: 12, md: 6 }}>
+                <Controller
+                  name="description"
+                  control={control}
+                  rules={{ required: true }}
+                  render={({ field }) => (
+                    <Textarea
+                      {...field}
+                      label="Description"
+                      description="Describe your venue"
+                      autosize
+                      minRows={8}
+                      maxRows={10}
+                      withAsterisk
+                    />
+                  )}
+                />
+                {errors.description && <Text>Description is required</Text>}
+              </Grid.Col>
+            </Grid>
+          </Stepper.Step>
+
+          <Stepper.Step label="Facilities" description="Available amenities">
+            <Grid gutter={40}>
+              {/* Step 1 - Facilities */}
+              <Grid.Col span={6}>
+                <Title order={4}>Facilities</Title>
+                <Text>Select features</Text>
+                <Stack>
                   <Controller
-                    name={`media[${index}].alt`}
+                    name="meta.wifi"
                     control={control}
+                    render={({ field: { value } }) => (
+                      <Switch
+                        label="Wifi"
+                        size="lg"
+                        onLabel="Yes"
+                        offLabel="No"
+                        checked={meta.wifi}
+                        onChange={(event) =>
+                          handleMetaChange("wifi", event.currentTarget.checked)
+                        }
+                      />
+                    )}
+                  />
+
+                  <Controller
+                    name="meta.parking"
+                    control={control}
+                    render={({ field: { value } }) => (
+                      <Switch
+                        label="Parking"
+                        size="lg"
+                        onLabel="Yes"
+                        offLabel="No"
+                        checked={meta.parking}
+                        onChange={(event) =>
+                          handleMetaChange(
+                            "parking",
+                            event.currentTarget.checked
+                          )
+                        }
+                      />
+                    )}
+                  />
+                  <Controller
+                    name="meta.breakfast"
+                    control={control}
+                    render={({ field: { value } }) => (
+                      <Switch
+                        label="Breakfast"
+                        size="lg"
+                        onLabel="Yes"
+                        offLabel="No"
+                        checked={meta.breakfast}
+                        onChange={(event) =>
+                          handleMetaChange(
+                            "breakfast",
+                            event.currentTarget.checked
+                          )
+                        }
+                      />
+                    )}
+                  />
+                  <Controller
+                    name="meta.pets"
+                    control={control}
+                    render={({ field: { value } }) => (
+                      <Switch
+                        label="Pets"
+                        size="lg"
+                        onLabel="Yes"
+                        offLabel="No"
+                        checked={meta.pets}
+                        onChange={(event) =>
+                          handleMetaChange("pets", event.currentTarget.checked)
+                        }
+                      />
+                    )}
+                  />
+                </Stack>
+              </Grid.Col>
+            </Grid>
+          </Stepper.Step>
+
+          <Stepper.Step label="Location" description="City, Country, Address">
+            <Grid gutter={40}>
+              {/* Step 2 - Location */}
+              <Grid.Col span={{ base: 12, sm: 6 }}>
+                <Stack>
+                  <SelectCityAndCountry control={control} setValue={setValue} />
+                  <Controller
+                    name="location.address"
+                    control={control}
+                    rules={{ required: false }}
                     render={({ field }) => (
-                      <Input.Wrapper
-                        label={`Describe your picture #${index + 1}`}
-                      >
-                        <Input
-                          leftSection={<IconTypography size={20} />}
-                          {...field}
-                        />
+                      <Input.Wrapper label="Address">
+                        <Input {...field} placeholder="Address" />
                       </Input.Wrapper>
                     )}
                   />
-                </Grid.Col>
-                <Grid.Col>
-                  {index === fields.length - 1 && fields.length < 6 && (
-                    <Grid.Col span={12}>
-                      <Button onClick={() => append({ url: "", alt: "" })}>
-                        Add media
-                      </Button>
-                    </Grid.Col>
-                  )}
-                </Grid.Col>
-              </Grid>
-            ))}
-          </Grid.Col>
+                </Stack>
+              </Grid.Col>
+            </Grid>
+          </Stepper.Step>
 
-          <Grid.Col>
+          <Stepper.Step label="Media" description="Pictures">
+            <Grid gutter={40}>
+              {/* Step 3 - Pictures */}
+              <Grid.Col>
+                <Title order={4}>Pictures</Title>
+                <Text>Up to 6 pictures. </Text>
+                {fields.map((field, index) => (
+                  <Grid key={field.id} gutter={10}>
+                    <Grid.Col span={{ base: 12, md: 6 }}>
+                      <Stack>
+                        <Controller
+                          name={`media[${index}].url`}
+                          control={control}
+                          rules={{ required: "URL is required" }} // Add required rule with a message
+                          render={({ field, fieldState: { error } }) => (
+                            <Input.Wrapper label={`Picture URL #${index + 1}`}>
+                              <Input
+                                leftSection={<IconPhoto size={20} />}
+                                placeholder="Enter media URL"
+                                {...field}
+                              />
+                              {error && <Text>{error.message}</Text>}
+                            </Input.Wrapper>
+                          )}
+                        />
+                        <Button
+                          variant="outline"
+                          color="red"
+                          fullWidth
+                          onClick={() => remove(index)}
+                          disabled={fields.length === 1}
+                        >
+                          Remove
+                        </Button>
+                      </Stack>
+                    </Grid.Col>
+                    <Grid.Col span={{ base: 12, md: 6 }}>
+                      <Controller
+                        name={`media[${index}].alt`}
+                        control={control}
+                        render={({ field }) => (
+                          <Input.Wrapper
+                            label={`Describe your picture #${index + 1}`}
+                          >
+                            <Input
+                              leftSection={<IconTypography size={20} />}
+                              {...field}
+                            />
+                          </Input.Wrapper>
+                        )}
+                      />
+                    </Grid.Col>
+                    <Grid.Col>
+                      {index === fields.length - 1 && fields.length < 6 && (
+                        <Grid.Col span={12}>
+                          <Button onClick={() => append({ url: "", alt: "" })}>
+                            Add media
+                          </Button>
+                        </Grid.Col>
+                      )}
+                    </Grid.Col>
+                  </Grid>
+                ))}
+              </Grid.Col>
+            </Grid>
+          </Stepper.Step>
+
+          <Stepper.Completed>
+            <Text>All steps completed - you're ready to submit the form</Text>
+          </Stepper.Completed>
+        </Stepper>
+
+        <Group justify="center" mt="xl">
+          <Button variant="default" onClick={prevStep} disabled={active === 0}>
+            Back
+          </Button>
+          {active === 3 ? (
             <Button type="submit">
               {venueId ? "Update Venue" : "Create Venue"}
             </Button>
-          </Grid.Col>
-        </Grid>
+          ) : (
+            <Button onClick={nextStep}>Next step</Button>
+          )}
+        </Group>
       </form>
     </>
   );
